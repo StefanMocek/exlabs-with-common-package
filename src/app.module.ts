c150a6a27247a37623537d5a6d96a4b50c198583
Stefan_M <105449172+StefanMocek@users.noreply.github.com>
2023-06-17 19:31:57 +0000

    cors fix, server url fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,11 +19,7 @@ export class AppModule {
   constructor(public app: Application, private dbUri: string, private swwagerDocPath: string) {
     //basic settings
     app.set('trust proxy', true);
-    app.use(cors({
-      credentials: true,
-      origin: process.env.CLIENT_ORIGIN || '*',
-      optionsSuccessStatus: 200
-    }))
+    app.use(cors());
     app.use(express.urlencoded({extended: false}));
     app.use(express.json());
     app.use(cookieSession({

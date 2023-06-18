3a87f1a0d20b0a526a489e8f06e7a4c009e93715
Stefan_M <105449172+StefanMocek@users.noreply.github.com>
2023-06-18 21:15:09 +0000

    app update

--- src/app.ts
+++ src/app.ts
@@ -10,20 +10,21 @@ declare global {
     }
   }
 }
+
 const PORT = process.env.PORT || 3000;
 const swaggerDocPath = path.join(__dirname, "../../swagger.yaml");
 
-const boostrap = async () => {
-  //check if JWT_KEY is present if not app wont start
-  //real mongo DB will be connected here for tests purpose
+const bootstrap = async () => {
   if (!process.env.MONGO_URL) {
-    throw new Error("MONGO_URL is require");
+    throw new Error("MONGO_URL is required");
   }
+
   const app = new AppModule(express(), process.env.MONGO_URL, swaggerDocPath);
   await app.start();
+
   app.app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
   });
 };
 
-boostrap();
+bootstrap();
